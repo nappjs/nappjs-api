42dbdcf4c7c87c5378ba25f180f7aa5c161baf91
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2018-01-05 05:27:53 +0100

    Update default port to 80

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const register = napp => {
 };
 
 const start = napp => {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 80;
   napp.locals.api.listen(port, err => {
     if (err) {
       console.log(`failed to start listening on ${port}, err: ${err}`);
